c10b550460982ffd48cb5b1fe3dbc07d429cdf8b
meistplay <meistplay@gmail.com>
2026-04-10 18:29:58 +0300

    Реализована функция get_zen_quote в utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
+import requests
+from config import API_URL
 
+
+def get_zen_quote():
+    """Получает случайную цитату с GitHub Zen API"""
+    try:
+        response = requests.get(API_URL)
+        if response.status_code == 200:
+            return response.text
+        return "Не удалось получить цитату."
+    except requests.exceptions.RequestException:
+        return "Ошибка сети."
